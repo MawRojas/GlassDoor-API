a69240237703dacc1500b4336ad114db8d11808a
MawRojas <m.aur@live.com.mx>
2018-09-10 09:31:45 -0400

    Added cors to server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const cors = require('cors');
+
+app.use(cors());
 
 const port = process.env.PORT || 3030; 
 app.listen(port);
